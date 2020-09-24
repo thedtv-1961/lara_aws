9f63d511f416e4f4d28253fa708592fdd645813a
Dam Trieu Van The <dam.trieu.van.the@sun-asterisk.com>
2020-09-24 09:52:58 +0700

    Create Product

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProductRequest;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        $products = Product::all()->sortByDesc('id');
+
+        return view('product.index', [
+            'products' => $products,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('product.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(ProductRequest $request)
+    {
+        $result = Product::create($request->post());
+
+        if ($result) {
+            return redirect()->route('product.index');
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(product $product)
+    {
+        //
+    }
+}
